3cd99b37534cfb89bddeee559f9bfbae394eba07
Kemerson Maciel <131490497+kemersonMaciel@users.noreply.github.com>
2024-03-17 21:54:56 -0300

    alteração de estrutura na paginaInicial

---
+++ js/paginaInicial.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.querySelectorAll('.news');
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    if (slideIndex < 0) { slideIndex = slides.length - 1; }
+    for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+}
+    slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlides();
+}
+
+    showSlides();
